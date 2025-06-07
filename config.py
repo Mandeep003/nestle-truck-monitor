3081054a7bdff5099ea4c8e123cdee9352dfa1a7
Mandeep003 <Mandeepbawa6862@gmail.com>
2025-06-06 23:47:10 -0600

    backend api changes

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
+import streamlit as st
+
 def get_user_role(password):
-    if password == "master123":
-        return "MasterUser"
-    elif password == "scm123":
-        return "SCM"
-    elif password == "gate123":
-        return "Gate"
-    elif password == "parking123":
-        return "Parking"
-    else:
-        return None
+    roles = {
+        "master123": "MasterUser",
+        "scm2025": "SCM",
+        "gate123": "Gate",
+        "parking123": "Parking"
+    }
+    return roles.get(password, None)
+
+def get_api_key():
+    return st.secrets["AIRTABLE_API_KEY"]
